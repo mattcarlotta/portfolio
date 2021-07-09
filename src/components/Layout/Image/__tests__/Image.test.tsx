73528e204035359d51adbd01368102c9f5a5d8ac
Matt <carlotta.matt@gmail.com>
2021-07-09 12:32:40 -0700

    [tests]: Update jest and fix broken tests

--- src/components/Layout/Image/__tests__/Image.test.tsx
+++ src/components/Layout/Image/__tests__/Image.test.tsx
@@ -1,8 +1,6 @@
 import { mount } from "enzyme";
-import { act } from "react-dom/test-utils";
+import { waitForAct } from "@noshot/utils";
 import Image from "../index";
-
-const flushPromises = () => new Promise(setImmediate);
 
 const onClick = jest.fn();
 const handleImageLoaded = jest.fn();
@@ -34,9 +32,8 @@ describe("Image", () => {
   });
 
   it("calls handleImageLoaded on load", async () => {
-    await act(async () => {
+    await waitForAct(() => {
       wrapper.find("img").props().onLoad();
-      await flushPromises();
       wrapper.update();
 
       expect(handleImageLoaded).toHaveBeenCalledTimes(1);
@@ -44,9 +41,8 @@ describe("Image", () => {
   });
 
   it("calls handleImageLoaded on failure and displays the broken link icon", async () => {
-    await act(async () => {
+    await waitForAct(() => {
       wrapper.find("img").props().onError();
-      await flushPromises();
       wrapper.update();
 
       expect(handleImageLoaded).toHaveBeenCalledTimes(1);
