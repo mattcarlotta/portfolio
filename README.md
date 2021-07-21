9e5170bd877ddd37d006689fb6250a5a01079486
Matt <carlotta.matt@gmail.com>
2021-07-21 09:19:33 -0700

    [core]: Drop support for IE11

--- README.md
+++ README.md
@@ -7,5 +7,3 @@ Current browser support:
 <img style="margin-right: 5px;" src="https://i.imgur.com/h1mIqac.png" alt="chrome.png" />
 <img style="margin-right: 5px;" src="https://i.imgur.com/tuZ5q64.png" alt="firefox.png" />
 <img style="margin-right: 5px;" src="https://i.imgur.com/mlO6osU.png" alt="safari.png" />
-<img style="margin-right: 5px;" src="https://i.imgur.com/0fyCByq.png" alt="samsung.png" />
-<img style="margin-right: 5px;" src="https://i.imgur.com/mD6SztK.png" alt="ie11.png" />
--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const { NormalModuleReplacementPlugin } = require("webpack");
-const { resolve } = require("path");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const address = require("address");
@@ -8,25 +6,12 @@ const { analyze, PORT } = process.env;
 
 console.log(`Remote URL http://${address.ip()}:${PORT}`);
 
-const polyfillsDir = "./src/polyfills/index.js";
-
 module.exports = {
   webpack5: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
   webpack(config, { isServer }) {
-    /* add framer motion support for IE11 */
-    const originalEntry = config.entry;
-    config.entry = async () => {
-      const entries = await originalEntry();
-
-      if (entries["main.js"] && !entries["main.js"].includes(polyfillsDir))
-        entries["main.js"].unshift(polyfillsDir);
-
-      return entries;
-    };
-
     /* adds custom plugins to client and server */
     config.plugins.push(
       ...[
@@ -37,11 +22,6 @@ module.exports = {
               ? "../analyze/server.html"
               : "./analyze/client.html",
           }),
-        !isServer &&
-          new NormalModuleReplacementPlugin(
-            /^framer-motion$/,
-            resolve(__dirname, "src/polyfills/framerMotionWrapper.js"),
-          ),
       ].filter(Boolean),
     );
 
