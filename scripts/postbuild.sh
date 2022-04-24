77fef119ec252df61491a62687bfb9a895228a0d
Matt <carlotta.matt@gmail.com>
2022-04-23 22:29:01 -0700

    chore: revert build script

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -eux
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-echo "$DIR"
-HTMLFILES=$(/usr/bin/find $DIR/../.next/server/pages -type f -name "*.html")
+HTMLFILES=$(/usr/bin/find $PWD/.next/server/pages -type f -name "*.html")
 DATE=$(/bin/date -u +"%FT%TZ")
 
 for file in $HTMLFILES; do
   line=$(/bin/grep -n 'og:updated_time' $file)
   if [ ! -z "$line" ]; then
     /usr/bin/perl -pi -e "s/time.+?(?=>)/time\" content=\"$DATE\"\//g;" ${file}
-    echo "$file updated"
+    echo "$file built at $DATE"  
   fi
 done
