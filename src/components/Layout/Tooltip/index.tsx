c8ef08bb7fb04c3fd0e3ac61edff379543ea3c4e
Matt <carlotta.matt@gmail.com>
2022-06-09 19:25:52 -0700

    fix: leverage pseudo elements for tooltips

--- src/components/Layout/Tooltip/index.tsx
+++ src/components/Layout/Tooltip/index.tsx
@@ -1,69 +1,12 @@
-import { useEffect, useRef, useState } from 'react'
 import type { Children } from '~types'
+import styles from './Tooltip.module.scss'
 
 export type ToolTipProps = Children & { title?: string }
 
 export default function Tooltip({ children, title }: ToolTipProps) {
-  const [isMounted, setMounted] = useState(false)
-  const tooltipRef = useRef<HTMLDivElement | null>(null)
-  const tooltipArrowRef = useRef<HTMLDivElement | null>(null)
-
-  const handleShowTooltip = () => {
-    /* istanbul ignore else */
-    if (tooltipRef.current && tooltipArrowRef.current) {
-      tooltipRef.current.style.display = 'block'
-      const { width } = tooltipRef.current.getBoundingClientRect()
-      tooltipRef.current.style.translate = `-${Math.round(width / 4.75)}px`
-      tooltipRef.current.style.top = '82%'
-      tooltipRef.current.style.opacity = '1'
-
-      // tooltipArrowRef.current.style.top = '89%'
-      // tooltipArrowRef.current.style.translate = '130%'
-      // tooltipArrowRef.current.style.transform = 'rotate(45deg)'
-      // tooltipArrowRef.current.style.opacity = '1'
-      // tooltipArrowRef.current.style.display = 'block'
-    }
-  }
-
-  const handleHideTooltip = () => {
-    /* istanbul ignore else */
-    if (tooltipRef.current && tooltipArrowRef.current) {
-      tooltipRef.current.style.opacity = '0'
-      tooltipRef.current.style.display = ''
-
-      // tooltipArrowRef.current.style.opacity = '0'
-      // tooltipArrowRef.current.style.display = ''
-    }
-  }
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  return isMounted ? (
-    <div data-testid="container" className="relative">
-      <div
-        role="presentation"
-        data-testid="tooltip-child"
-        onFocus={handleShowTooltip}
-        onBlur={handleHideTooltip}
-        onMouseEnter={handleShowTooltip}
-        onMouseLeave={handleHideTooltip}
-      >
-        {children}
-      </div>
-      <div
-        data-testid="tooltip"
-        className="whitespace-no-wrap fixed z-50 hidden rounded bg-primary-25 px-4 py-2 font-plain text-tiny text-white opacity-0"
-        ref={tooltipRef}
-      >
-        {title}
-      </div>
-      <div
-        data-testid="tooltip-arrow"
-        className="fixed z-40 hidden h-3 w-3 bg-primary-25 opacity-0"
-        ref={tooltipArrowRef}
-      />
+  return (
+    <div className={styles.tooltip} data-title={title}>
+      {children}
     </div>
-  ) : null
+  )
 }
